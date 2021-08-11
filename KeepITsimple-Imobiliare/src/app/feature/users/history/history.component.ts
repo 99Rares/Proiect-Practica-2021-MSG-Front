c17fce43508eed67f0e3e6ab5c33be14e6e392f3
Rareș Dan <46723989+99Rares@users.noreply.github.com>
2021-08-11 21:01:41 +0300

    history works

--- KeepITsimple-Imobiliare/src/app/feature/users/history/history.component.ts
+++ KeepITsimple-Imobiliare/src/app/feature/users/history/history.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ApartmentDetails} from "../../apartments/model/apartment.data";
+import {WishlistService} from "../../wishlist/wishlist.service";
 
 @Component({
   selector: 'app-history',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HistoryComponent implements OnInit {
 
-  constructor() { }
+  constructor(private wishlistService: WishlistService) { }
+
+  apartments: ApartmentDetails[] = []
 
   ngOnInit(): void {
+    this.wishlistService.toHistory();
+    this.wishlistService.loadWishlist()
+    this.wishlistService.history.subscribe(data => {
+      this.apartments = data;
+    });
+    // this.apartments= JSON.parse(<string>localStorage.getItem("my_history")); //get them back
+  }
+  toWishlist(id: number) {
+    this.wishlistService.toWishlist(id);
   }
 
 }
