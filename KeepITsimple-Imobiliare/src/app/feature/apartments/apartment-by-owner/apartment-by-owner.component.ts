1e1c39d2e207370af58dce33359077beeef0c34b
Gombos Krisztian <krisztian_gombos@yahoo.com>
2021-08-12 11:08:14 +0300

    added owner statistics page

---
+++ KeepITsimple-Imobiliare/src/app/feature/apartments/apartment-by-owner/apartment-by-owner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-apartment-by-owner',
+  templateUrl: './apartment-by-owner.component.html',
+  styleUrls: ['./apartment-by-owner.component.scss']
+})
+export class ApartmentByOwnerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
